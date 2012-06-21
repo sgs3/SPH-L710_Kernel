f72e14a75d2e94f4352816e4b0548906c639d503
Scott Warner <Tortel1210@gmail.com>
2012-06-21 08:47:51 -0400

    Initial commit of SPH-L710 source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Tue Jun 19 12:18:06 KST 2012"
+#define LINUX_COMPILE_BY "shina.lee"
+#define LINUX_COMPILE_HOST "Sprint10"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
